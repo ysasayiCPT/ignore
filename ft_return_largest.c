d846f445ea6a21920686bf5a868b0b1080caa30f
ysasayi <ysasayi@student.wethinkcode>
2020-07-16 21:04:02 +0200

    initial commit

---
+++ ft_return_largest.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int ft_return_largest(int* n);
+int main()
+{
+	int numbers[] = {12,52,78,80,32,33,52};
+	printf("the largest of all: %d", ft_return_largest(numbers));
+	return 0;
+}
+int ft_return_largest(int* n)
+{
+	int i =0;
+	int keeper = n[0];
+	while(i<7)
+	{
+		if(n[i] > keeper)
+		{
+			keeper = n[i];
+		}
+		i++;
+	}
+	return keeper;
+}
